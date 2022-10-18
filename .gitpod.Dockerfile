15b3cf541dd9d76160f6f2aeb4a09dc4ab04db41
Andrii Iv. [Yolkstudio] <hi@digitalandy.eu>
2022-10-18 02:36:38 +0200

    Revert "wp bedrock clean: try sh scripts"
    This reverts commit 0681dc175b924047821f0ad487fd47cb60831dc5.

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -2,5 +2,3 @@ FROM gitpod/workspace-mysql
 
 USER gitpod
 RUN brew install wp-cli php@8.0 && brew unlink php && brew link php@8.0
-
-RUN 
--- .gitpod/config.sh
+++
@@ -1,5 +0,0 @@
-# bash
-echo 'export PATH="/home/linuxbrew/.linuxbrew/opt/php@8.0/bin:$PATH"' >> /home/gitpod/.bash_profile
-echo 'export PATH="/home/linuxbrew/.linuxbrew/opt/php@8.0/sbin:$PATH"' >> /home/gitpod/.bash_profile
-wp package install aaemnnosttv/wp-cli-dotenv-command:^2.0
-
